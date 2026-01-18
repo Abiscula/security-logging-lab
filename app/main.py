c5449b9937d114982e3fc527d08d349f9e093f85
Victor Abiscula <victor.abiscula@tensai.com.br>
2026-01-18 02:00:16 -0300

    feat: Adicionado rotas simuladas para testes de log

--- app/main.py
+++ app/main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from app.DTO import LoginRequest, ActionRequest
+from app.events import (
+  log_auth_attempt,
+  log_sensitive_action,
+  log_system_error
+)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "API funcionando!"}
+
+@app.post("/auth/login")
+def login(data: LoginRequest, request: Request):
+  ip = request.client.host
+  agent = request.headers.get("user-agent", "unknown")
+
+  success = data.password == "123456"
+  log_auth_attempt(data.username, ip, agent, success)
+
+  return {"success": success}
+
+
+@app.post("/admin/action")
+def admin_action(data: ActionRequest, request: Request):
+  ip = request.client.host
+  agent = request.headers.get("user-agent", "unknown")
+
+  log_sensitive_action(data.action, ip, agent)
+  return {"status": "action registered"}
+
+
+@app.get("/system/error")
+def system_error(request: Request):
+  ip = request.client.host
+  agent = request.headers.get("user-agent", "unknown")
+
+  try:
+    1 / 0
+  except Exception as e:
+    log_system_error(str(e), ip, agent)
+    return {"error": "system failure simulated"}
