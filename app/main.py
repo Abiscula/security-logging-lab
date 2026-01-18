e8c5c2aec8920c9babdd3bd20707303e3a55cdc0
Victor Abiscula <victorabiscula@live.com>
2026-01-18 16:39:37 -0300

    refactor: move rotas da API para módulo dedicado usando APIRouter

--- app/main.py
+++ app/main.py
@@ -1,42 +1,6 @@
-from fastapi import FastAPI, Request
-
-from app.DTO import LoginRequest, ActionRequest
-from app.events import (
-  log_auth_attempt,
-  log_sensitive_action,
-  log_system_error
-)
+from fastapi import FastAPI
+from app.routes import router
 
 app = FastAPI()
 
-
-@app.post("/auth/login")
-def login(data: LoginRequest, request: Request):
-  ip = request.client.host
-  agent = request.headers.get("user-agent")
-
-  success = data.password == "123456"
-  log_auth_attempt(data.username, ip, agent, success)
-
-  return {"success": success}
-
-
-@app.post("/admin/action")
-def admin_action(data: ActionRequest, request: Request):
-  ip = request.client.host
-  agent = request.headers.get("user-agent")
-
-  log_sensitive_action(data.action, ip, agent)
-  return {"status": "action registered"}
-
-
-@app.get("/system/error")
-def system_error(request: Request):
-  ip = request.client.host
-  agent = request.headers.get("user-agent")
-
-  try:
-    1 / 0
-  except Exception as e:
-    log_system_error(str(e), ip, agent)
-    return {"error": "system failure simulated"}
+app.include_router(router)
