f3c5c2465d9fb1eab34cf79f8cbe93daeba238a3
Victor Abiscula <victorabiscula@live.com>
2026-01-25 23:36:04 -0300

    feat: Adiciona script para criar bucket security-logs no LocalStack

---
+++ app/scripts/create_bucket.py
@@ -0,0 +1,9 @@
+from app.services.s3_client import get_s3_client
+
+BUCKET_NAME = "security-logs"
+
+s3 = get_s3_client()
+
+s3.create_bucket(Bucket=BUCKET_NAME)
+
+print(f"Bucket '{BUCKET_NAME}' criado com sucesso")
